d86fa2e5b20dc7c7e2cb412c8a41c8f38708e88f
Kohei Nishiwaki <koheinishiwaki117@gmail.com>
2025-10-26 13:48:28 +0900

    バージョン変更によりCatモデルにRansackの検索対象属性を定義

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,9 @@
 class Cat < ApplicationRecord
+
+    # Ransackが検索対象として許可する属性（カラム）のリストを定義します。
+    # このリストに含まれていないカラムは検索できなくなります。
+    def self.ransackable_attributes(auth_object = nil)
+        # エラーメッセージが提案したリスト
+        ["age", "created_at", "id", "name", "updated_at"]
+    end
 end
